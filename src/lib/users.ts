1b035c3082598030ea4980bf8c9d514f71eae4e7
Xilef7 <felix@sembly.com>
2026-01-09 00:30:16 +0700

    Fix getGuest

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,26 +1,27 @@
 import 'server-only'
 import { cache } from 'react'
-import { CustomerId, GuestId, TabId } from './types'
+import { convertStrToDbOwnerId, CustomerId, GuestId, TabId } from './types'
 import { clerkClient } from '@clerk/nextjs/server'
 import { fetchQuery } from 'convex/nextjs'
 import { api } from '@/convex/_generated/api'
 
 export const getCustomer = cache(async (id: CustomerId) => {
   const client = await clerkClient()
-  const [, customerId] = id.split('.')
+  const { value } = convertStrToDbOwnerId(id)
 
-  const user = await client.users.getUser(customerId)
+  const user = await client.users.getUser(value)
 
   return user
 })
 
 export const getGuest = cache(async (id: GuestId) => {
-  const [, tabId, guestId] = id.split('.')
+  const { value } = convertStrToDbOwnerId(id)
+  const [tabId] = value.split('.')
   const tab = await fetchQuery(api.tabs.get, { tabId: tabId as TabId })
   if (typeof tab === 'object' && 'error' in tab) {
     throw new Error('TAB_NOT_FOUND')
   }
-  const name = tab.guestNames[guestId]
+  const name = tab.guestNames[value]
   if (name === undefined) {
     throw new Error('GUEST_NOT_FOUND')
   }
