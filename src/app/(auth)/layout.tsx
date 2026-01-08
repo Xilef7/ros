86cedaa12aca57fc6e8240f1044944469a0e6934
Xilef7 <felix@sembly.com>
2026-01-08 15:04:58 +0700

    Fix padding

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <div className="flex-1 flex flex-col justify-center">{children}</div>
+  return (
+    <div className="flex-1 flex flex-col justify-center p-4">{children}</div>
+  )
 }
